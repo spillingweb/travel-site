929cd4d954ac5281ef307336dc68ed8bcc3107a0
spillingweb <khspilling@gmail.com>
2024-03-28 14:31:05 -0500

    counted to four

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,12 @@ import Button from "./UI/Button";
 const Header: React.FC = () => {
   return (
     <>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
       <header>
         <img src={headerIcon} />
         <Button>Get in Touch</Button>
