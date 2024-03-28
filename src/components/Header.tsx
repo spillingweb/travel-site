14c4711dfd00eab201a4551b5dea8c2970db2835
spillingweb <khspilling@gmail.com>
2024-03-28 14:32:14 -0500

    counted to seven

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,9 @@ const Header: React.FC = () => {
       <li>2</li>
       <li>3</li>
       <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
     </ul>
       <header>
         <img src={headerIcon} />
