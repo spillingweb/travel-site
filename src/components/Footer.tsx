5547d171e1ec784f82760ea4382672eb235acd73
spillingweb <khspilling@gmail.com>
2024-04-01 09:50:48 -0500

    Completed footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,17 @@
 import Button from "./UI/Button";
+import Wrapper from "./UI/Wrapper";
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <p>
-        Copyright &copy; 2019 Clear View Escapes. All rights reserved.{" "}
-        <Button color="orange">Get in Touch</Button>
-      </p>
+    <footer className="site-footer">
+      <Wrapper>
+        <p>
+          <span className="site-footer__text">
+            Copyright &copy; 2019 Clear View Escapes. All rights reserved.{" "}
+          </span>
+          <Button color="orange">Get in Touch</Button>
+        </p>
+      </Wrapper>
     </footer>
   );
 };
