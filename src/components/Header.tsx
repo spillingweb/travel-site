532b3e9e529d7fcd1bdf3c8c25c5b11167147a67
spillingweb <khspilling@gmail.com>
2024-04-02 12:17:13 -0500

    styled mobile nav menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import headerIcon from "../assets/images/icons/clear-view-escapes.svg";
 import LargeHero from "./LargeHero";
 import Navbar from "./Navbar";
@@ -5,17 +6,34 @@ import Button from "./UI/Button";
 import Wrapper from "./UI/Wrapper";
 
 const Header: React.FC = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  
+  function handleToggleMenu() {
+    setMenuVisible((prevState) => !prevState);
+  }
+
+  let siteHeaderClasses = "site-header";
+  let menuContentClasses = "site-header__menu-content";
+
+  if (menuVisible) {
+    siteHeaderClasses += " site-header--is-expanded"
+    menuContentClasses += " site-header__menu-content--is-visible";
+  }
+
   return (
     <>
-      <header className="site-header">
+      <header className={siteHeaderClasses}>
         <Wrapper>
           <div className="site-header__logo">
             <img src={headerIcon} />
           </div>
 
-          <div className="site-header__menu-icon"></div>
+          <div
+            className="site-header__menu-icon"
+            onClick={handleToggleMenu}
+          ></div>
 
-          <div className="site-header__menu-content">
+          <div className={menuContentClasses}>
             <div className="site-header__btn-container">
               <Button>Get in Touch</Button>
             </div>
