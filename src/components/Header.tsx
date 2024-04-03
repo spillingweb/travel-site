a499de7059ff584ef65a32a384e55e26d4c81bee
spillingweb <khspilling@gmail.com>
2024-04-03 10:50:53 -0500

    finished styling navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,16 +7,18 @@ import Wrapper from "./UI/Wrapper";
 
 const Header: React.FC = () => {
   const [menuVisible, setMenuVisible] = useState(false);
-  
+
   function handleToggleMenu() {
     setMenuVisible((prevState) => !prevState);
   }
 
+  let menuIconClasses = "site-header__menu-icon"
   let siteHeaderClasses = "site-header";
   let menuContentClasses = "site-header__menu-content";
 
   if (menuVisible) {
-    siteHeaderClasses += " site-header--is-expanded"
+    menuIconClasses += " site-header__menu-icon--close-x"
+    siteHeaderClasses += " site-header--is-expanded";
     menuContentClasses += " site-header__menu-content--is-visible";
   }
 
@@ -28,10 +30,9 @@ const Header: React.FC = () => {
             <img src={headerIcon} />
           </div>
 
-          <div
-            className="site-header__menu-icon"
-            onClick={handleToggleMenu}
-          ></div>
+          <div className={menuIconClasses} onClick={handleToggleMenu}>
+            <div className="site-header__menu-icon__middle"></div>
+          </div>
 
           <div className={menuContentClasses}>
             <div className="site-header__btn-container">
